62ba2e29f96b182e65d04ea9402188a9bb9539ae
Samuelayala2212 <samuel.ayala02@usa.edu.co>
2025-02-18 07:17:26 -0500

    Add files via upload

---
+++ ex2.py
@@ -0,0 +1,5 @@
+a=input("Introducir producto: ")
+b= int(input("Introducir precio del producto: "))
+print("El producto",a,"con el descuento del 15% queda en:",b*0.85,"$")
+print("El ahorro es de:",b*0.15,"$")
+
